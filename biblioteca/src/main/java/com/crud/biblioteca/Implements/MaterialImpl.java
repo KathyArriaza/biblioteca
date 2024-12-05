e8408bb166fe7b1104bcd0af7ef56e3e3b760f43
Noemy Arriaza <noemy.arriaza@outlook.com>
2024-12-04 22:04:56 -0600

    agregando ejemplo de impl

---
+++ biblioteca/src/main/java/com/crud/biblioteca/Implements/MaterialImpl.java
@@ -0,0 +1,4 @@
+package com.crud.biblioteca.Implements;
+
+public class MaterialImpl {
+}
---
+++ biblioteca/src/main/java/com/crud/biblioteca/repositories/MaterialRepository.java
@@ -0,0 +1,9 @@
+package com.crud.biblioteca.repositories;
+
+import com.crud.biblioteca.entites.Material;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaterialRepository extends CrudRepository<Material, Long> {
+}
