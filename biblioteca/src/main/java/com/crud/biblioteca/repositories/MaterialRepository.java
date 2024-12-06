a0c7ba737fe09744ed407f4f13eb29db1324d652
Abdías Hernández <abdiashdz034@outlook.com>
2024-12-05 21:13:51 -0600

    Agregando la consulta a ejecutar para ejecutar el procedimiento

--- biblioteca/src/main/java/com/crud/biblioteca/repositories/MaterialRepository.java
+++ biblioteca/src/main/java/com/crud/biblioteca/repositories/MaterialRepository.java
@@ -1,11 +1,14 @@
 package com.crud.biblioteca.repositories;
 
 import com.crud.biblioteca.entites.Material;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MaterialRepository extends CrudRepository<Material, Long> {
 
-String callConsultaMateriales(String tipoMaterial) throws Exception;
+    @Query(value = "CALL PROC$OBTENER_MATERIALES(:tipoMaterial)", nativeQuery = true)
+    String callConsultaMateriales(@Param("tipoMaterial") String tipoMaterial) throws Exception;
 }
