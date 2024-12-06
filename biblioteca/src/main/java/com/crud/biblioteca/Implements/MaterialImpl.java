64289abcb864fd423e24402176daddd09a04f47f
Linares-29 <yancipineda32@gmail.com>
2024-12-05 20:56:58 -0600

    Agregando cambios a MaterialImpl y MaterialRepository

--- biblioteca/src/main/java/com/crud/biblioteca/Implements/MaterialImpl.java
+++ biblioteca/src/main/java/com/crud/biblioteca/Implements/MaterialImpl.java
@@ -1,4 +1,74 @@
 package com.crud.biblioteca.Implements;
 
-public class MaterialImpl {
+import com.crud.biblioteca.entites.Material;
+import com.crud.biblioteca.repositories.MaterialRepository;
+
+import java.util.Optional;
+
+public class MaterialImpl implements MaterialRepository {
+
+    public String callConsultaMateriales(String tipoMaterial) throws Exception {
+        return null;
+    }
+
+    @Override
+    public <S extends Material> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Material> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Material> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public Iterable<Material> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Material> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Material entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Material> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
+
--- biblioteca/src/main/java/com/crud/biblioteca/repositories/MaterialRepository.java
+++ biblioteca/src/main/java/com/crud/biblioteca/repositories/MaterialRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MaterialRepository extends CrudRepository<Material, Long> {
+
+String callConsultaMateriales(String tipoMaterial) throws Exception;
 }
